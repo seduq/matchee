a14e5fdc03c0674ccafdab4c38d1a2ef6bb530fa
Gustavo Ceccon <gustavofceccon@gmail.com>
2025-01-14 00:25:33 -0300

    Bare Template

---
+++ Raids/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Raids.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Trials/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Trials.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
